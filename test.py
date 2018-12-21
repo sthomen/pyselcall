7c58916e52d50e355f33f5032de77af92276590a
Staffan Thomen <duck@shangtai.net>
2018-12-21 21:22:48 +0200

    Tone generator test update

--- test.py
+++ test.py
@@ -1,14 +1,39 @@
-from tonegen import Square, Saw, Triangle, Sine
+from tonegen import Square, Ramp, Saw, Triangle, Sine
 from time import sleep
 
-square = Square(1000)
-saw = Saw(1000)
-triangle = Triangle(1000)
-sine = Sine(1000)
+frequency = 1000
+sample_rate = 10000
+depth = 6
 
-for i,j,k,l in zip(square, saw, triangle, sine):
-	print(" " * int(i/512) + "square")
-	print(" " * int(j/512) + "saw")
-	print(" " * int(k/512) + "triangle")
-	print(" " * int(l/512) + "sine")
-	sleep(0.001)
+classes = (Square, Ramp, Saw, Triangle, Sine)
+
+waves=[]
+
+for wavetype in classes:
+	waves.append(wavetype(frequency, sample_rate, depth))
+
+
+sample=0
+
+strlen = (1 << depth) + 1
+
+for i,j,k,l,m in zip(*waves):
+	if (sample % int(sample_rate / frequency)) == 0:
+		print('-' * strlen)
+	else:
+
+		buf=[' '] * strlen
+
+		for index, char in enumerate(str(sample)):
+			buf[index]=char
+
+#		buf[i]='s'
+		buf[j]='r'
+		buf[k]='w'
+#		buf[l]='t'
+#		buf[m]='i'
+
+		print(''.join(buf))
+
+	sample+=1
+	sleep(0.1)
