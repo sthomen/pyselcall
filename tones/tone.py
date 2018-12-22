02aa694ddeb4f55aac46c9f859b042ad3c2a8f8d
Staffan Thomen <duck@shangtai.net>
2018-12-22 15:23:57 +0200

    Add signed support to the tone base class

--- tones/tone.py
+++ tones/tone.py
@@ -1,6 +1,18 @@
 class Tone(object):
-	def __init__(self, frequency, rate=44100, bits=16):
+	def __init__(self, frequency, rate=44100, bits=16, signed=False):
 		self.frequency=frequency
-		self.rate=rate
-		self.bits=bits
+		self.rate = rate
+		self.bits = bits
+		self.signed = signed
+
 		self.full = (1 << self.bits)-1
+
+
+		if self.signed:
+			self.high = self.full / 2
+			self.mid = 0
+			self.low = -self.high
+		else:
+			self.high = self.full
+			self.mid = self.full / 2
+			self.low = 0
