7d16dc767723eaffc97d4a98fd125ba8816c06c5
Staffan Thomen <duck@shangtai.net>
2018-12-22 20:18:44 +0200

    Make the signedness of the output adjustable, but enforce it for wave files. Add inline documentation.

--- melody.py
+++ melody.py
@@ -3,7 +3,23 @@ import wave
 from tones import Tone, Sine
 
 class Melody(object):
-	def __init__(self, code, waveform=Sine, rate=44100, bits=16):
+	"""
+	Melody generator for a given waveform, sample rate and bit depth.
+
+	This class takes the given "code"--an iterable with tuples of <frequency>
+	and <duration> (in milliseconds)--and renders it into audio samples from
+	the given waveform (a sine wave by default).
+
+	It contains a very simple wav-file writer that can output the sequence of
+	frequencies into a wav-file.
+
+	See also:
+	
+	See the tones module for available tone generators and the selcall module
+	for generating frequencies for a variety of selcall squelch codes.
+	"""
+
+	def __init__(self, code, waveform=Sine, rate=44100, bits=16, signed=True):
 		if not issubclass(waveform, Tone):
 			raise ValueError("The waveform parameter must be a a class derived from tones.Tone")
 
@@ -13,8 +29,14 @@ class Melody(object):
 		self.bits = bits
 
 	def __iter__(self):
+		"""
+		This method allows you to run a Melody object as a generator of audio
+		samples, it will yield values as samples from the selcted waveform in
+		the frequency and duration specified by the "code" until the latter is
+		exhausted.
+		"""
 		for frequency, duration in self.code:
-			waveform = iter(self.waveform(frequency, self.rate, self.bits, signed=True))
+			waveform = iter(self.waveform(frequency, self.rate, self.bits, signed=self.signed))
 
 			frames = int((duration / 1000) * self.rate)
 
@@ -22,6 +44,11 @@ class Melody(object):
 				yield int(next(waveform))
 
 	def wave(self, fn):
+		"""
+		This runs the generator in __iter__ and produces a wav file
+		"""
+		self.signed = True
+
 		with wave.open(fn, 'wb') as fp:
 			fp.setnchannels(1)
 			fp.setsampwidth(self.bits//8)
@@ -29,6 +56,6 @@ class Melody(object):
 			fp.setcomptype('NONE', 'Not Compressed')
 
 			for frame in self:
-				fp.writeframesraw(frame.to_bytes(self.bits//8, 'little', signed=True))
+				fp.writeframesraw(frame.to_bytes(self.bits//8, 'little', signed=self.signed))
 
 			fp.close()
