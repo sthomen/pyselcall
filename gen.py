636ab5e3f0f3cb4a16396f967821be50ce177fcf
Staffan Thomen <duck@shangtai.net>
2018-12-27 21:23:08 +0200

    Give the gen and play scripts user friendly argument parsing for no real reason

--- gen.py
+++ gen.py
@@ -1,10 +1,21 @@
 import sys
+from argparse import ArgumentParser
 
 from selcall import Code, Durations
 from melody import Melody
 
-args=tuple(int(a) for a in sys.argv[1:])
+parser = ArgumentParser()
+parser.add_argument('numbers', metavar='N', type=int, nargs='+', help='SelCall numbers')
+parser.add_argument('-w', metavar='name', dest='waveform', default='Sine', help='the waveform to use; one of Sine, Square, Saw, Ramp, defaults to \'Sine\'')
+parser.add_argument('-a', metavar='value', dest='attennuation', type=int, default=8, help='output volume attennuation, 1..n where 1 is full volume, defaults to 8')
+parser.add_argument('-f', metavar='filename', dest='filename', default='output.wav', help='output filename, defaults to \'output.wav\'')
 
-melody = Melody(Code(args, duration=Durations.ERICSSON))
+args = parser.parse_args()
+try:
+	waveform = getattr(sys.modules[__name__], args.waveform)
+except AttributeError:
+	quit("Invalid waveform: {}".format(args.waveform))
 
-melody.wave('output.wav')
+melody = Melody(Code(args.numbers, duration=Durations.ERICSSON), waveform=waveform)
+
+melody.wave(args.filename)
--- play.py
+++ play.py
@@ -1,17 +1,29 @@
 import sys
 
+from argparse import ArgumentParser
 import pyaudio
 
 from selcall import Code, Durations
 from melody import Melody
+from tones import *
 
-args=tuple(int(a) for a in sys.argv[1:])
+parser = ArgumentParser()
+parser.add_argument('numbers', metavar='N', type=int, nargs='+', help='SelCall numbers')
+parser.add_argument('-w', metavar='name', dest='waveform', default='Sine', help='the waveform to use; one of Sine, Square, Saw, Ramp, defaults to \'Sine\'')
+parser.add_argument('-a', metavar='value', dest='attennuation', type=int, default=8, help='output volume attennuation, 1..n where 1 is full volume, defaults to 8')
 
-melody = Melody(Code(args, duration=Durations.ERICSSON))
+args = parser.parse_args()
+try:
+	waveform = getattr(sys.modules[__name__], args.waveform)
+except AttributeError:
+	quit("Invalid waveform: {}".format(args.waveform))
+
+melody = Melody(Code(args.numbers, duration=Durations.ERICSSON), waveform=waveform)
 
 # load all the sample data because pyaudio seems to require a length, and our generators work on-demand
 data=bytes()
 for frame in melody:
+	frame//=args.attennuation
 	data+=frame.to_bytes(melody.bits//8, 'little', signed=melody.signed)
 
 pa = pyaudio.PyAudio()
