67ca405ae37cde2dc371eb2bad1602c9c9ee0c82
Staffan Thomen <duck@shangtai.net>
2018-12-28 22:36:35 +0200

    Add the ability to specify an arbitrary duration, as well as mutual exclusivity with the -e (Ericsson) option.

--- pyselcall/scripts/pyselcall.py
+++ pyselcall/scripts/pyselcall.py
@@ -3,10 +3,16 @@ from argparse import ArgumentParser
 import pyselcall
 
 def run():
-	parser = ArgumentParser()
+	parser = ArgumentParser(epilog='If the -f option is not given, then this script will attempt to play the sounds through pyaudio.')
 
 	parser.add_argument('numbers', metavar='N', type=int, nargs='+', help='numbers 0-9')
 	parser.add_argument('-w', dest='waveform', default='Sine', help='the waveform to use; one of Sine, Square, Saw, Ramp, defaults to \'Sine\'')
+
+	group = parser.add_mutually_exclusive_group()
+
+	group.add_argument('-d', dest='duration', type=int, default=33, help='duration in milliseconds')
+	group.add_argument('-e', dest='duration', action='store_const', const=pyselcall.Durations.ERICSSON, help='Use Ericsson 700ms, 100ms, 100ms... durations')
+
 	parser.add_argument('-a', dest='attennuation', type=int, default=8, help='output volume attennuation, 1..n where 1 is full volume, defaults to 8')
 	parser.add_argument('-f', dest='filename', help='output audio to a wav file with the given filename')
 
@@ -16,7 +22,7 @@ def run():
 	except AttributeError:
 		quit("Invalid waveform: {}".format(args.waveform))
 
-	melody = pyselcall.Melody(pyselcall.Code(args.numbers, duration=pyselcall.Durations.ERICSSON), waveform=waveform)
+	melody = pyselcall.Melody(pyselcall.Code(args.numbers, duration=args.duration), waveform=waveform)
 
 	if args.filename:
 		melody.wave(args.filename, args.attennuation)
